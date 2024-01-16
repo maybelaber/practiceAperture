d650ae84e2038ad28986d8cbf81cb60b3bae0e1f
maybelaber <danyandandrew8@gmail.com>
2024-01-16 18:10:34 +0200

    adaptive nav-btn (part 2)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+/* Nav icon */
 const navBtn = document.querySelector('.header__nav-btn');
 const navIcon = document.querySelector('.nav-icon');
+const row = document.querySelector('.header__row');
 
 navBtn.addEventListener('click', function() {
     navIcon.classList.toggle('nav-icon--active');
+    row.classList.toggle('header__row--mobile');
+    document.body.classList.toggle("no-scroll");
 });
