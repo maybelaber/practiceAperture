e02e07b0dc58bb93dd6a822d2364eb2e80705b76
maybelaber <danyandandrew8@gmail.com>
2024-04-03 19:26:55 +0300

    fix bugs in mobile navigation (header__row)

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ const navIcon = document.querySelector('.nav-icon');
 const row = document.querySelector('.header__row');
 const navAccent = document.querySelector('.nav__item--accent');
 
-navBtn.addEventListener('click', function() {
+const rowTransitionDuration = 480;
+
+navBtn.addEventListener('click', function () {
     navIcon.classList.toggle('nav-icon--active');
     row.classList.toggle('header__row--mobile');
     document.body.classList.toggle("no-scroll");
-    navAccent.classList.toggle("nav__item--accent")
+    navAccent.classList.toggle("nav__item--accent");
+    row.classList.remove('transition-none');
+
+    setTimeout(function () {
+        row.classList.toggle('scroll-y');
+        row.classList.add('transition-none');
+    }, rowTransitionDuration);
 });
